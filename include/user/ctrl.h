9a6ee44295aeaef827ebced24ac3d27861f7eaf0
Jeffrey Pfau <jeffrey@endrift.com>
2016-08-08 20:46:42 -0700

    Add sceCtrlSetActuator

--- include/user/ctrl.h
+++ include/user/ctrl.h
@@ -68,6 +68,12 @@ typedef struct SceCtrlRapidFireRule {
 	unsigned int Break;
 } SceCtrlRapidFireRule;
 
+/** Structure to pass as argument to ::sceCtrlSetActuator */
+typedef struct SceCtrlActuator {
+	int enable;  //!< Enable the actuator vibration
+	int unk;     //!< Unknown
+} SceCtrlActuator;
+
 /**
  * Set the controller mode.
  *
@@ -150,6 +156,16 @@ int sceCtrlSetRapidFire(int port, int idx, const SceCtrlRapidFireRule* pRule);
  * @return 0, <0 on error.
  */
 int sceCtrlClearRapidFire(int port, int idx);
+
+/**
+ * Control the actuator (vibrate) on paired controllers.
+ *
+ * @param[in] port - use 1 for the first paired controller, etc.
+ * @param[in] state - see ::SceCtrlActuator
+ *
+ * @return 0, <0 on error.
+ */
+int sceCtrlSetActuator(int port, const SceCtrlActuator* pState);
 
 #ifdef __cplusplus
 }
