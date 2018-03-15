bd38b71f0d04ea2837b0af9f710ed832ca72f4bb
CelesteBlue-dev <celesteblue.dev@gmail.com>
2018-03-15 18:36:14 +0100

    Add psp2/sblacmgr.h

---
+++ include/user/sblacmgr.h
@@ -0,0 +1,16 @@
+#ifndef _PSP2_SBLACMGR_H_
+#define _PSP2_SBLACMGR_H_
+
+#include <psp2/types.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int _sceSblACMgrIsGameProgram(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _PSP2_SBLACMGR_H_ */
