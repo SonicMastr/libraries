3c9f7d2ec5618c6fead581a20d6f991eb1840e2a
Davee <git@davee.pro>
2018-10-28 16:17:16 +0000

    add ksceAppMgrKillProcess

---
+++ include/kernel/appmgr.h
@@ -0,0 +1,21 @@
+#ifndef _PSP2_KERNEL_APPMGR_H_
+#define _PSP2_KERNEL_APPMGR_H_
+
+#include <psp2kern/types.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief       Kill a process.
+ * @param[in]   pid The process to kill.
+ * @return      Zero on success, else < 0.
+ */
+int sceAppMgrKillProcess(SceUID pid);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _PSP2_KERNEL_APPMGR_H_ */
