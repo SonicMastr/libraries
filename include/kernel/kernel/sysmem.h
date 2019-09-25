vitasdk/vita-headers

fixed ksceKernelFindClassByName

include/psp2kern/kernel/sysmem.h
@@ -282,7 +282,7 @@ SceClass *ksceKernelGetUidMemBlockClass(void);
 int ksceKernelCreateClass(SceClass *cls, const char *name, void *uidclass, size_t itemsize, SceClassCallback create, SceClassCallback destroy);
 int ksceKernelDeleteUserUid(SceUID pid, SceUID user_uid);
 int ksceKernelDeleteUid(SceUID uid);
-int ksceKernelFindClassByName(const char name, SceClass **cls);
+int ksceKernelFindClassByName(const char *name, SceClass **cls);
 
 int ksceKernelSwitchVmaForPid(SceUID pid);
 

SonicMastr/libraries

include/kernel/kernel/sysmem.h
@@ -277,7 +277,7 @@ SceClass *sceKernelGetUidMemBlockClass(void);
 int sceKernelCreateClass(SceClass *cls, const char *name, void *uidclass, size_t itemsize, SceClassCallback create, SceClassCallback destroy);
 int sceKernelDeleteUserUid(SceUID pid, SceUID user_uid);
 int sceKernelDeleteUid(SceUID uid);
-int sceKernelFindClassByName(const char name, SceClass **cls);
+int sceKernelFindClassByName(const char *name, SceClass **cls);
 
 int sceKernelSwitchVmaForPid(SceUID pid);
 