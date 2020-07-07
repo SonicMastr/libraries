4274ec840e263fc35f985144fd6e96ff210c8b5d
GrapheneCt <57592952+GrapheneCt@users.noreply.github.com>
2020-07-07 07:10:20 -0400

    Add psp2/bgapputil.h
    From https://github.com/GrapheneCt/Vita-PoC/blob/master/bgapputil.h

---
+++ include/user/bgapputil.h
@@ -0,0 +1,26 @@
+#ifndef _DOLCESDK_PSP2_BGAPPUTIL_H_
+#define _DOLCESDK_PSP2_BGAPPUTIL_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Errors
+ */
+#define SCE_BGAPPUTIL_ERROR_INVALID_ARG 0x80106501
+
+/**
+ * Start BG application (eboot2.bin)
+ *
+ * @param[in] mode - must be 0
+ *
+ * @return 0 on success, <0 otherwise.
+ */
+int sceBgAppUtilStartBgApp(int mode);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* _DOLCESDK_PSP2_BGAPPUTIL_H_ */
