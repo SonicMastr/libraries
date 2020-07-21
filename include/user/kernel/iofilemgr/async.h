cd9c1ead26ec0c0ab590cc570862e6d5d99195ed
GrapheneCt <57592952+GrapheneCt@users.noreply.github.com>
2020-07-21 17:39:17 -0400

    Add async IO functions for userland
    All credits go to https://github.com/GrapheneCt/

---
+++ include/user/kernel/iofilemgr/async.h
@@ -0,0 +1,103 @@
+#ifndef _DOLCESDK_PSP2_KERNEL_IOFILEMGR_ASYNC_H_
+#define _DOLCESDK_PSP2_KERNEL_IOFILEMGR_ASYNC_H_
+
+#include <psp2/kernel/threadmgr.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Async IO operations are scheduled automatically, similar to FIOS2.
+ * You can make calls to sceIoOpenAsync(), sceIoReadAsync(),
+ * sceIoCloseAsync() right after each other, they will be put in a schedule
+ * and performed automatically.
+ *
+ * All async IO functions return UID of operation handle, not file descriptor UID.
+ */
+
+/**
+  * Cancel an asynchronous operation.
+  *
+  * @param opHandle - The operation handle to perform cancel on.
+  *
+  * @return < 0 on error. If operation has been canceled(finished) already, returns 0x80010002
+  */
+int sceIoCancel(SceUID opHandle);
+
+/**
+  * Complete an asynchronous operation.
+  *
+  * @param opHandle - The operation handle to complete.
+  *
+  * @return < 0 on error.
+  */
+int sceIoComplete(SceUID opHandle);
+
+/**
+  * Complete multiple asynchronous operations.
+  *
+  * @param asyncParam - Array of ::SceIoAsyncParam representing IO operations to complete. retVal member must have opHandle value assigned to it.
+  * @param numOfParam - Number of ::SceIoAsyncParam structs in asyncParam array
+  * @param numOfCompleted - Number of operations that has been completed successfully
+  *
+  * @return < 0 on last encountered error.
+  */
+int sceIoCompleteMultiple(SceIoAsyncParam* asyncParam, int numOfParam, int* numOfCompleted);
+
+/**
+  * Wait until asynchronous operation has been finished.
+  *
+  * @param opHandle - The operation handle to wait for.
+  *
+  * @return < 0 on error.
+  */
+inline
+int sceIoWaitAsync(SceUID opHandle)
+{
+	int ret = sceKernelWaitEvent(opHandle, 1, NULL, NULL, NULL);
+	if (ret == 0)
+		return sceIoComplete(opHandle);
+	else
+		return ret;
+}
+
+/**
+  * Wait until asynchronous operation has been finished with callbacks.
+  *
+  * @param opHandle - The operation handle to wait for.
+  *
+  * @return < 0 on error.
+  */
+inline
+int sceIoWaitAsyncCB(SceUID opHandle)
+{
+	int ret = sceKernelWaitEventCB(opHandle, 1, NULL, NULL, NULL);
+	if (ret == 0)
+		return sceIoComplete(opHandle);
+	else
+		return ret;
+}
+
+/**
+  * Poll asynchronous operation status.
+  *
+  * @param opHandle - The operation handle to poll status for.
+  *
+  * @return < 0 on error or if operation is not finished.
+  */
+inline
+int sceIoPollAsync(SceUID opHandle)
+{
+	int ret = sceKernelPollEvent(opHandle, 1, NULL, NULL);
+	if (ret == 0)
+		return sceIoComplete(opHandle);
+	else
+		return ret;
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif	/* _DOLCESDK_PSP2_KERNEL_IOFILEMGR_ASYNC_H_ */
--- include/user/kernel/iofilemgr/stat.h
+++ include/user/kernel/iofilemgr/stat.h
@@ -2,7 +2,7 @@
 #define _DOLCESDK_PSP2_KERNEL_IOFILEMGR_STAT_H_
 
 #include <psp2/kernel/types.h>
-#include <psp2/types.h>
+#include <psp2/kernel/iofilemgr/async.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -81,6 +81,50 @@ int sceIoGetstatByFd(SceUID fd, SceIoStat *buf);
   * @return < 0 on error.
   */
 int sceIoChstatByFd(SceUID fd, const SceIoStat *buf, unsigned int cbit);
+
+/**
+ * Make a directory file (asynchronous)
+ *
+ * @param dir - The path to the directory
+ * @param mode - Access mode (One or more ::SceIoAccessMode).
+ * @param asyncParam - parameters related to async operation.
+ *
+ * @return A non-negative integer is a valid op handle, anything else an error
+ */
+SceUID sceIoMkdirAsync(const char *dir, SceMode mode, SceIoAsyncParam* asyncParam);
+
+/**
+ * Remove a directory file (asynchronous)
+ *
+ * @param path - Removes a directory file pointed by the string path
+ * @param asyncParam - parameters related to async operation.
+ *
+ * @return A non-negative integer is a valid op handle, anything else an error
+ */
+SceUID sceIoRmdirAsync(const char *path, SceIoAsyncParam* asyncParam);
+
+/**
+  * Get the status of a file (asynchronous)
+  *
+  * @param file - The path to the file.
+  * @param stat - A pointer to a ::SceIoStat structure.
+  * @param asyncParam - parameters related to async operation.
+  *
+  * @return A non-negative integer is a valid op handle, anything else an error
+  */
+SceUID sceIoGetstatAsync(const char *file, SceIoStat *stat, SceIoAsyncParam* asyncParam);
+
+/**
+  * Change the status of a file (asynchronous)
+  *
+  * @param file - The path to the file.
+  * @param stat - A pointer to a ::SceIoStat structure.
+  * @param bits - Bitmask defining which bits to change.
+  * @param asyncParam - parameters related to async operation.
+  *
+  * @return A non-negative integer is a valid op handle, anything else an error
+  */
+SceUID sceIoChstatAsync(const char *file, SceIoStat *stat, int bits, SceIoAsyncParam* asyncParam);
 
 #ifdef __cplusplus
 }
