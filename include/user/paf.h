5d6d2e8e2906bfe35093be10d6932c9a26e8a6e1
GrapheneCt <57592952+GrapheneCt@users.noreply.github.com>
2020-08-28 19:49:26 -0400

    Rename sce_paf_private -> sce_paf
    Due to official name found in old version of paf module by Graphene.

--- include/user/paf.h
+++ include/user/paf.h
@@ -7,26 +7,26 @@
 extern "C" {
 #endif
 
-void sce_paf_private_free(void *ptr);
-void *sce_paf_private_malloc(SceSize size);
-void *sce_paf_private_bzero(void *ptr, SceSize num);
-void *sce_paf_private_memchr(const void *ptr, int value, SceSize num);
-int sce_paf_private_memcmp(const void *ptr1, const void *ptr2, SceSize num);
-int sce_paf_private_bcmp(const void *ptr1, const void *ptr2, SceSize num);
-void *sce_paf_private_memcpy(void *destination, const void *source, SceSize num);
-void *sce_paf_private_memcpy2(void *destination, const void *source, SceSize num);
-void *sce_paf_private_memmove(void *destination, const void *source, SceSize num);
-void *sce_paf_private_bcopy(void *destination, const void *source, SceSize num);
-void *sce_paf_private_memset(void *ptr, int value, SceSize num);
-int sce_paf_private_snprintf(char *s, SceSize n, const char *format, ...);
-int sce_paf_private_strcasecmp(const char *str1, const char *str2);
-char *sce_paf_private_strchr(const char *str, int character);
-int sce_paf_private_strcmp(const char *str1, const char *str2);
-size_t sce_paf_private_strlen(const char *str);
-int sce_paf_private_strncasecmp(const char *str1, const char *str2, SceSize num);
-int sce_paf_private_strncmp(const char *str1, const char *str2, SceSize num);
-char *sce_paf_private_strncpy(char *destination, const char *source, SceSize num);
-char *sce_paf_private_strrchr(const char *str, int character);
+void sce_paf_free(void *ptr);
+void *sce_paf_malloc(SceSize size);
+void *sce_paf_bzero(void *ptr, SceSize num);
+void *sce_paf_memchr(const void *ptr, int value, SceSize num);
+int sce_paf_memcmp(const void *ptr1, const void *ptr2, SceSize num);
+int sce_paf_bcmp(const void *ptr1, const void *ptr2, SceSize num);
+void *sce_paf_memcpy(void *destination, const void *source, SceSize num);
+void *sce_paf_memcpy2(void *destination, const void *source, SceSize num);
+void *sce_paf_memmove(void *destination, const void *source, SceSize num);
+void *sce_paf_bcopy(void *destination, const void *source, SceSize num);
+void *sce_paf_memset(void *ptr, int value, SceSize num);
+int sce_paf_snprintf(char *s, SceSize n, const char *format, ...);
+int sce_paf_strcasecmp(const char *str1, const char *str2);
+char *sce_paf_strchr(const char *str, int character);
+int sce_paf_strcmp(const char *str1, const char *str2);
+size_t sce_paf_strlen(const char *str);
+int sce_paf_strncasecmp(const char *str1, const char *str2, SceSize num);
+int sce_paf_strncmp(const char *str1, const char *str2, SceSize num);
+char *sce_paf_strncpy(char *destination, const char *source, SceSize num);
+char *sce_paf_strrchr(const char *str, int character);
 
 #ifdef __cplusplus
 }
