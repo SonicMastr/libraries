e06f2583aa165c79a322e015fd6a94e1255b0018
GrapheneCt <57592952+GrapheneCt@users.noreply.github.com>
2020-11-21 14:13:04 -0500

    Add type SILK for audiodec and audioenc

---
+++ include/user/audiodec.h
@@ -0,0 +1,9 @@
+#ifndef _DOLCESDK_PSP2_AUDIODEC_H_
+#define _DOLCESDK_PSP2_AUDIODEC_H_
+
+#include_next <audiodec.h>
+
+/* Audio decoder type */
+#define SCE_AUDIODEC_TYPE_SILK (0x1007U)
+
+#endif /* _DOLCESDK_PSP2_AUDIODEC_H_ */
---
+++ include/user/audioenc.h
@@ -0,0 +1,9 @@
+#ifndef _DOLCESDK_PSP2_AUDIOENC_H_
+#define _DOLCESDK_PSP2_AUDIOENC_H_
+
+#include_next <audioenc.h>
+
+/* Audio encoder type */
+#define SCE_AUDIOENC_TYPE_SILK (0x2007U) /* Only use with sceAudioencCreateEncoderResident() */
+
+#endif /* _DOLCESDK_PSP2_AUDIOENC_H_ */
